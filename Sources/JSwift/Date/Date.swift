475ce422cd87990987d1611c0fa9f2967218a594
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-07-18 00:07:29 -0700

    Update Date.swift

--- Sources/JSwift/Date/Date.swift
+++ Sources/JSwift/Date/Date.swift
@@ -96,9 +96,99 @@ public extension Date {
         
         return Time(hour: hour, minute: minute, second: second)
     }
+    
+    var friendlyDescription: String {
+        let calendar = Calendar.current
+        let now = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        if calendar.isDateInToday(self) {
+            return "Today"
+        } else if calendar.isDateInTomorrow(self) {
+            return "Tomorrow"
+        }
+        
+        let components = calendar.dateComponents([.day, .weekOfYear, .month, .year], from: now, to: self)
+        let dayDifference = components.day!
+        let weekDifference = components.weekOfYear!
+        let monthDifference = components.month!
+        let yearDifference = components.year!
+        
+        let weekdayToday = calendar.component(.weekday, from: now)
+        let weekdayTarget = calendar.component(.weekday, from: self)
+        
+        // Yesterday
+        if dayDifference == -1 {
+            return "Yesterday"
+        }
+        
+        // Correct handling of same week but different days
+        if weekDifference == 0 {
+            if dayDifference > 0 {
+                if weekdayTarget > weekdayToday {
+                    return "Next \(dateFormatter.string(from: self))"
+                }
+            } else if dayDifference < 0 {
+                if weekdayTarget < weekdayToday {
+                    return "Last \(dateFormatter.string(from: self))"
+                }
+            } else {
+                return dateFormatter.string(from: self)
+            }
+        }
+        
+        // Next and last weekday handling
+        if weekDifference == 1 && weekdayTarget <= weekdayToday {
+            return "Next \(dateFormatter.string(from: self))"
+        } else if weekDifference == -1 && weekdayTarget >= weekdayToday {
+            return "Last \(dateFormatter.string(from: self))"
+        }
+        
+        // Relative weeks
+        if weekDifference == 1 {
+            return "Next week"
+        } else if weekDifference == -1 {
+            return "Last week"
+        } else if weekDifference > 1 && weekDifference < 4 {
+            return "In \(weekDifference) weeks"
+        } else if weekDifference < -1 && weekDifference > -4 {
+            return "\(-weekDifference) weeks ago"
+        }
+        
+        // Months
+        if monthDifference == 1 {
+            return "Next month"
+        } else if monthDifference == -1 {
+            return "Last month"
+        } else if monthDifference > 1 && monthDifference < 12 {
+            return "In \(monthDifference) months"
+        } else if monthDifference < -1 && monthDifference > -12 {
+            return "\(-monthDifference) months ago"
+        }
+        
+        // Years
+        if yearDifference == 1 {
+            return "Next year"
+        } else if yearDifference == -1 {
+            return "Last year"
+        } else if yearDifference > 1 {
+            return "In \(yearDifference) years"
+        } else if yearDifference < -1 {
+            return "\(-yearDifference) years ago"
+        }
+        
+        // Default to the day of the week
+        return dateFormatter.string(from: self)
+    }
 }
 
 public extension Date {
+    
+    func advanceBy(_ value: Int) -> Self {
+        Calendar.current.date(byAdding: .day, value: value, to: self) ?? self
+    }
     
     func isSame(granularity: Calendar.Component, as date: Date) -> Bool {
         Calendar.current.isDate(self, equalTo: date, toGranularity: granularity)
